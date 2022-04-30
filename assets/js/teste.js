c97d5b58d37424379db52d22c5205ac7494a464e
Racuminho <103468409+Racuminho@users.noreply.github.com>
2022-04-29 23:40:20 -0300

    Add files via upload

---
+++ assets/js/teste.js
@@ -0,0 +1,36 @@
+//var preço = 2;  //constante não pode ser alterada
+//var desconto = 0.2;
+
+//const preço = 2;
+
+//var total = preço - desconto;
+
+//function soma (a,b){
+    
+    //return a+b;
+//}
+
+//soma (3, 5);
+
+//console.log ("Olá, Mundo!");
+
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
+
+
+
